fe607e68d1b9a04c565554e5f1bc654efac00e12
CrisRocuant <mundorocuant@gmail>
2022-12-02 11:49:16 -0300

    buscador de ciudad

--- js/fetch.js
+++ js/fetch.js
@@ -1,25 +1,40 @@
-const input = document.querySelector('#searchInput')
-const userList = document.querySelector('#users')
+const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
-window.addEventListener('DOMContentLoaded', async () => {
-    const data = await loadUsers()
-    renderUsers(data.data)
+const cities = [];
 
-})
+fetch(endpoint)
+  .then(blob => blob.json())
+  .then(data => cities.push(...data))
 
-async function loadUsers(){
-    const response = await fetch('https://fakerapi.it/api/v1/users?_quantity=100')
-    return await response.json()
+function findMatches(wordToMatch, cities) {
+  return cities.filter(place => {
+    const regex = new RegExp(wordToMatch, 'gi');
+    return place.city.match(regex) || place.state.match(regex)
+  });
 }
 
-input.addEventListener('keyup', e => {
-    console.log(input.value)
-})
-                                                     //revisar esta Linia//
-const createUserItems = users => users.map(users => `<li>hola</>`).join('')
-
-function renderUsers(users) {
-    const itemsString = createUserItems(users)
-    userList.innerHTML = itemsString
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
-    
+
+function displayMatches() {
+  const matchArray = findMatches(this.value, cities);
+  const html = matchArray.map(place => {
+    const regex = new RegExp(this.value, 'gi');
+    const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
+    const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
+    return `
+      <li>
+        <span class="name">${cityName}, ${stateName}</span>
+        <span class="population">${numberWithCommas(place.population)}</span>
+      </li>
+    `;
+  }).join('');
+  suggestions.innerHTML = html;
+}
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
